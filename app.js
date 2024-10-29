e6b4a121f202864b626a635d8b5a91c63b0eb3b3
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-10-29 09:44:44 +0500

    added

---
+++ app.js
@@ -0,0 +1,357 @@
+let allData = {
+    toyota: {
+        toyotaCarolla: {
+            img: "public/toyota-corolla.png",
+            name: "Toyota Corolla",
+            model: 2023,
+            color: "white",
+            cityName: "Karachi",
+            price: "PKR 3,000,000",
+        },
+        toyotaCamry: {
+            img: "public/toyota-camry.png",
+            name: "Toyota Camry",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,800,000",
+        },
+        toyotaHighLander: {
+            img: "public/toyota-high-lander.png",
+            name: "Toyota Highlander",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 4,000,000",
+        },
+        toyotaRav4: {
+            img: "public/toyota-rav4.png",
+            name: "Toyota RAV4",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,500,000",
+        },
+        toyotaYaris: {
+            img: "public/toyota-yaris.png",
+            name: "Toyota Yaris",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,500,000",
+        },
+        toyotaLandCruiser: {
+            img: "public/toyota-land-cruiser.png",
+            name: "Toyota Land Cruiser",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,400,000",
+        },
+        toyotaHilux: {
+            img: "public/toyota-hilux.png",
+            name: "Toyota Hilux",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,400,000",
+        },
+        toyotaFortuner: {
+            img: "public/toyota-fortuner.png",
+            name: "Toyota Fortuner",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,100,000",
+        },
+        toyotaCorollaCross: {
+            img: "public/toyota-corolla-cross.png",
+            name: "Toyota Corolla Cross",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,400,000",
+        },
+        toyotaPrius: {
+            img: "public/toyota-pruis.png",
+            name: "Toyota Prius",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 5,000,000",
+        },
+        toyota4Runner: {
+            img: "public/toyota-4-runner.png",
+            name: "Toyota 4Runner",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 7,000,000",
+        },
+        toyotaSupra: {
+            img: "public/toyota-supra.png",
+            name: "Toyota Supra",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,300,000",
+        },
+        toyotaCHR: {
+            img: "public/toyota-chr.png",
+            name: "Toyota CH-R",
+            model: 2021,
+            color: "black",
+            cityName: "Karachi",
+            price: "PKR 2,100,000",
+        }
+    },
+    hyundai: {
+        hyundaiElantra: {
+            img: "public/hyundai-elantra.png",
+            name: "Hyundai Elantra",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 2,000,000",
+        },
+        hyundaiTucson: {
+            img: "public/hyundai-tucson.png",
+            name: "Hyundai Tucson",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 3,000,000",
+        },
+        hyundaiKona: {
+            img: "public/hyundai-kona.png",
+            name: "Hyundai Kona",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 4,000,000",
+        },
+        hyundaiVenue: {
+            img: "public/hyundai-venue.png",
+            name: "Hyundai Venue",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 3,500,000",
+        },
+        hyundaiAccent: {
+            img: "public/hyundai-accent.png",
+            name: "Hyundai Accent",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 2,600,000",
+        },
+        hyundaiSantaFe: {
+            img: "public/hyundai-santa-fe.png",
+            name: "Hyundai Santa Fe",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 2,999,999",
+        },
+        hyundaiGrandI10: {
+            img: "public/hyundai-grandi.png",
+            name: "Hyundai Grand i10",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 3,400,000",
+        },
+        hyundaiVerna: {
+            img: "public/hyundai-verna.png",
+            name: "Hyundai Verna",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 6,000,050",
+        },
+        hyundaiCreta: {
+            img: "public/hyundai-creta.png",
+            name: "Hyundai Creta",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 8,000,000",
+        },
+        hyundaiEON: {
+            img: "public/hyundai-eon.png",
+            name: "Hyundai EON",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 5,000,000",
+        },
+        hyundaiAvante: {
+            img: "public/hyundai-avante.png",
+            name: "Hyundai Avante",
+            model: 2021,
+            color: "black",
+            cityName: "Lahore",
+            price: "PKR 2,500,000",
+        }
+    },
+    suzuki: {
+        suzukiMehran: {
+            img: "public/suzuki-mehran.png",
+            name: "Suzuki Mehran",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 2,800,000",
+        },
+        suzukiCultus: {
+            img: "public/suzuki-cultus.png",
+            name: "Suzuki Cultus",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 2,300,000",
+        },
+        suzukiWagonR: {
+            img: "public/suzuki-wagon-r.png",
+            name: "Suzuki Wagon R",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 5,400,000",
+        },
+        suzukiSwift: {
+            img: "public/suzuki-swift.png",
+            name: "Suzuki Swift",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 4,000,000",
+        },
+        suzukiBaleno: {
+            img: "public/suzuki-baleno.png",
+            name: "Suzuki Baleno",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 2,500,000",
+        },
+        suzukiLiana: {
+            img: "public/suzuki-liana.png",
+            name: "Suzuki Liana",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 2,600,000",
+        },
+        suzukiJimny: {
+            img: "public/suzuki-jimny.png",
+            name: "Suzuki Jimny",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 4,500,000",
+        },
+        suzukiCiaz: {
+            img: "public/suzuki-ciaz.png",
+            name: "Suzuki Ciaz",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 2,400,000",
+        },
+        suzukiCross: {
+            img: "public/suzuki-cross.png",
+            name: "Suzuki Cross",
+            model: 2021,
+            color: "black",
+            cityName: "Bahawalpur",
+            price: "PKR 3,300,000",
+        }
+    }
+};
+
+// Populate the company dropdown
+let compay = document.getElementById('company');
+let model = document.getElementById('model');
+let cardRol = document.getElementById('cardRol');
+
+
+
+
+
+ for(let key in allData){
+    compay.innerHTML+=`<option value='${key}'>${key}</option>`
+    for(let key1 in allData[key]){
+    // console.log(allData[key][key1].model);
+    cardRol.innerHTML+= `<div class="col-12 col-lg-4 col-md-6 col-sm-12 cardcol">
+  
+    <div class=" home-card" style="width: 18rem;">
+      <img id='carImgs' src="${allData[key][key1].img}" alt="...">
+      <div class="card-body">
+             <h4 class="card-title">${allData[key][key1].name} ${allData[key][key1].model}</h4>    
+         <p class="card-title">${allData[key][key1].cityName}</p>
+                 <h5 class="card-title">${allData[key][key1].color}</h5>
+         <h3 class="card-title">${allData[key][key1].price}</h3>
+
+        <img src="img/Group 14png" alt="">
+      </div>
+    </div>
+
+  </div>`
+   
+
+
+    }
+  
+ }
+
+for (let key in allData) {
+    compay.innerHTML += `<option value='${key}'>${key}</option>`;
+}
+
+// Function to populate model dropdown based on selected company
+function selectCompany() {
+    model.innerHTML = `<option value="" selected disabled>select model</option>`;
+    for (let key1 in allData[compay.value]) {
+        model.innerHTML += `<option value='${key1}'>${allData[compay.value][key1].name}</option>`;
+    }
+}
+
+// Function to filter out car based on selected company and model
+function filtOutCar() {
+    cardRol.innerHTML = '';
+    let selectedCompany = compay.value;
+    let selectedModel = model.value;
+    if (selectedCompany && selectedModel) {
+        let carData = allData[selectedCompany][selectedModel];
+        cardRol.innerHTML += `<div class="col-12 col-lg-6 col-md-12 col-sm-12 cardcol ">
+            <div class="home-card " style="width: 18rem;">
+                <img id='carImgs' src="${carData.img}" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${carData.name} ${carData.model}</h4>
+                    <p class="card-title">${carData.cityName}</p>
+                    <h5 class="card-title">${carData.color}</h5>
+                    <h3 class="card-title">${carData.price}</h3>
+                </div>
+            </div>
+        </div>
+         <div class="col-12 col-lg-6 col-md-6 col-sm-12 cardcol ">
+           
+           <img width='100%' class='gif' src="public/sale-dealer-woman-brand-colors.png" alt="">
+                
+            </div>
+        </div>`
+        
+        
+        
+        
+       
+        
+        
+        
+    }
+}
+
+compay.addEventListener('change', selectCompany);
+model.addEventListener('change', filtOutCar);
